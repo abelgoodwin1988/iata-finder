9771b4f09b8a6d3f36bcefffeb2e1e9f230ab393
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-01 20:04:28 -0700

    add config and conn handling

---
+++ deployments/gcp_postgres/credentials.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// ConnectionConfiguration is a struct type for loading in our db connection information
+type ConnectionConfiguration struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Dbname   string `json:"dbname"`
+}
+
+// GetConnectionConfiguration returns the crentials config for db connection
+func GetConnectionConfiguration() ConnectionConfiguration {
+	file, _ := os.Open("connection.config.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := ConnectionConfiguration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
+}

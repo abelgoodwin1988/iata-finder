3500ef5b7a961402290ce9be5df4366a70e2893e
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-01 18:32:07 -0700

    add starting points for gcp db deploy

---
+++ deployments/gcp_postgres/db.go
@@ -0,0 +1,14 @@
+// Package db handles the initial set up and population of the database
+package db
+
+/**
+ * db is the main handler and entry point for the program to:
+ *	connect to the database
+ *	delete any schema/tables that exist in the place where
+	we'll be creating our datastore
+ *	populate the tables with the files found in the assets
+	directory
+*/
+func db() {
+
+}

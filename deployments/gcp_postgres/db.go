d1c1256c2d8a6a4068ead6c24c56d47ef39d67b0
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-01 20:02:39 -0700

    add conn to db

--- deployments/gcp_postgres/db.go
+++ deployments/gcp_postgres/db.go
@@ -1,5 +1,12 @@
-// Package db handles the initial set up and population of the database
-package db
+// Package main handles the initial set up and population of the database
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 /**
  * db is the main handler and entry point for the program to:
@@ -9,6 +16,30 @@ package db
  *	populate the tables with the files found in the assets
 	directory
 */
-func db() {
-
+func main() {
+	connCFG := GetConnectionConfiguration()
+	fmt.Printf("%v", connCFG)
+	clientCert := "../../certificates/client-cert.pem"
+	clientKey := "../../certificates/client-key.pem"
+	serverCA := "../../certificates/server-ca.pem"
+	psqlConn := fmt.Sprintf("sslmode=verify-ca sslrootcert=%s sslcert=%s sslkey=%s host=%s port=%s user=%s dbname=%s password=%s",
+		serverCA,
+		clientCert,
+		clientKey,
+		connCFG.Host,
+		connCFG.Port,
+		connCFG.User,
+		connCFG.Dbname,
+		connCFG.Password,
+	)
+	db, err := sql.Open("postgres", psqlConn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Connected Successfully")
 }

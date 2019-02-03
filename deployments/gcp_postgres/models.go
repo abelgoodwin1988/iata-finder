2dbac6b242a97b2a4acb7bdf20e464c463c2d2f7
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-02 23:32:22 -0700

    add models file, addtl super types, and methods

---
+++ deployments/gcp_postgres/models.go
@@ -0,0 +1,68 @@
+package main
+
+// Airline is a struct representing openflights data for
+//	IATA airline data
+type Airline struct {
+	ID       int
+	Name     string
+	Iata     string
+	Icao     string
+	Callsign string
+	Country  string
+	Active   string
+}
+
+// Airlines is a set of Airline
+type Airlines []Airline
+
+// Airport is a struct representing openflight data for
+//	IATA airport data
+type Airport struct {
+	ID                  int
+	Name                string
+	City                string
+	Country             string
+	Iata                string
+	Icao                string
+	Latitude            float64
+	Longitude           float64
+	Altitude            float64
+	Timezone            string
+	DaylightSavingsTime string
+	Tz                  string
+	TypeField           string
+	Source              string
+}
+
+// Airports is a set of Airport
+type Airports []Airport
+
+// Values returns values of Airline
+func (a Airline) Values() []interface{} {
+	return []interface{}{a.ID, a.Name, a.Iata, a.Icao, a.Callsign, a.Country, a.Active}
+}
+
+// Values returns values of Airport
+func (a Airport) Values() []interface{} {
+	return []interface{}{a.ID, a.Name, a.City, a.Country, a.Iata, a.Icao, a.Latitude, a.Longitude, a.Altitude, a.Timezone, a.DaylightSavingsTime, a.Tz, a.TypeField, a.Source}
+}
+
+// Values returns values for Airports
+func (a Airports) Values() [][]interface{} {
+	rtrn := [][]interface{}{}
+	for _, v := range a {
+		vals := v.Values()
+		rtrn = append(rtrn, vals)
+	}
+	return rtrn
+}
+
+// Values returns values for Airports
+func (a Airlines) Values() [][]interface{} {
+	rtrn := [][]interface{}{}
+	for _, v := range a {
+		vals := v.Values()
+		rtrn = append(rtrn, vals)
+	}
+	return rtrn
+}

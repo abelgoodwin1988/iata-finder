d40fcd4ecffbcde30c552c9b96f4614f704ff890
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2019-02-02 23:32:50 -0700

    use supertypes for airline(s) and airport(s)

--- deployments/gcp_postgres/credentials.go
+++ deployments/gcp_postgres/credentials.go
@@ -9,7 +9,7 @@ import (
 // ConnectionConfiguration is a struct type for loading in our db connection information
 type ConnectionConfiguration struct {
 	Host     string `json:"host"`
-	Port     string `json:"port"`
+	Port     uint16 `json:"port"`
 	User     string `json:"user"`
 	Password string `json:"password"`
 	Dbname   string `json:"dbname"`

481de02b0f558df6ea67893994a83f6e4068806c
Abel Goodwin <abelgoodwin1988@gmail.com>
2019-03-24 22:01:10 -0600

    updated pb file based on new msg's/srv's

--- deployments/iatafinder_rpc/iatafinder.pb.go
+++ deployments/iatafinder_rpc/iatafinder.pb.go
@@ -8,6 +8,8 @@ import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -21,6 +23,37 @@ var _ = math.Inf
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+
+type EmptyRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *EmptyRequest) Reset()         { *m = EmptyRequest{} }
+func (m *EmptyRequest) String() string { return proto.CompactTextString(m) }
+func (*EmptyRequest) ProtoMessage()    {}
+func (*EmptyRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ff5d7323532d9adb, []int{0}
+}
+
+func (m *EmptyRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_EmptyRequest.Unmarshal(m, b)
+}
+func (m *EmptyRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_EmptyRequest.Marshal(b, m, deterministic)
+}
+func (m *EmptyRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_EmptyRequest.Merge(m, src)
+}
+func (m *EmptyRequest) XXX_Size() int {
+	return xxx_messageInfo_EmptyRequest.Size(m)
+}
+func (m *EmptyRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_EmptyRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_EmptyRequest proto.InternalMessageInfo
 
 type Airport struct {
 	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
@@ -46,7 +79,7 @@ func (m *Airport) Reset()         { *m = Airport{} }
 func (m *Airport) String() string { return proto.CompactTextString(m) }
 func (*Airport) ProtoMessage()    {}
 func (*Airport) Descriptor() ([]byte, []int) {
-	return fileDescriptor_ff5d7323532d9adb, []int{0}
+	return fileDescriptor_ff5d7323532d9adb, []int{1}
 }
 
 func (m *Airport) XXX_Unmarshal(b []byte) error {
@@ -165,73 +198,201 @@ func (m *Airport) GetSource() string {
 	return ""
 }
 
-type Iata struct {
+type Airports struct {
+	Airport              []*Airport `protobuf:"bytes,1,rep,name=airport,proto3" json:"airport,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}   `json:"-"`
+	XXX_unrecognized     []byte     `json:"-"`
+	XXX_sizecache        int32      `json:"-"`
+}
+
+func (m *Airports) Reset()         { *m = Airports{} }
+func (m *Airports) String() string { return proto.CompactTextString(m) }
+func (*Airports) ProtoMessage()    {}
+func (*Airports) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ff5d7323532d9adb, []int{2}
+}
+
+func (m *Airports) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Airports.Unmarshal(m, b)
+}
+func (m *Airports) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Airports.Marshal(b, m, deterministic)
+}
+func (m *Airports) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Airports.Merge(m, src)
+}
+func (m *Airports) XXX_Size() int {
+	return xxx_messageInfo_Airports.Size(m)
+}
+func (m *Airports) XXX_DiscardUnknown() {
+	xxx_messageInfo_Airports.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Airports proto.InternalMessageInfo
+
+func (m *Airports) GetAirport() []*Airport {
+	if m != nil {
+		return m.Airport
+	}
+	return nil
+}
+
+type IATA struct {
 	Iata                 string   `protobuf:"bytes,1,opt,name=iata,proto3" json:"iata,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Iata) Reset()         { *m = Iata{} }
-func (m *Iata) String() string { return proto.CompactTextString(m) }
-func (*Iata) ProtoMessage()    {}
-func (*Iata) Descriptor() ([]byte, []int) {
-	return fileDescriptor_ff5d7323532d9adb, []int{1}
+func (m *IATA) Reset()         { *m = IATA{} }
+func (m *IATA) String() string { return proto.CompactTextString(m) }
+func (*IATA) ProtoMessage()    {}
+func (*IATA) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ff5d7323532d9adb, []int{3}
 }
 
-func (m *Iata) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Iata.Unmarshal(m, b)
+func (m *IATA) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_IATA.Unmarshal(m, b)
 }
-func (m *Iata) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Iata.Marshal(b, m, deterministic)
+func (m *IATA) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_IATA.Marshal(b, m, deterministic)
 }
-func (m *Iata) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Iata.Merge(m, src)
+func (m *IATA) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_IATA.Merge(m, src)
 }
-func (m *Iata) XXX_Size() int {
-	return xxx_messageInfo_Iata.Size(m)
+func (m *IATA) XXX_Size() int {
+	return xxx_messageInfo_IATA.Size(m)
 }
-func (m *Iata) XXX_DiscardUnknown() {
-	xxx_messageInfo_Iata.DiscardUnknown(m)
+func (m *IATA) XXX_DiscardUnknown() {
+	xxx_messageInfo_IATA.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Iata proto.InternalMessageInfo
+var xxx_messageInfo_IATA proto.InternalMessageInfo
 
-func (m *Iata) GetIata() string {
+func (m *IATA) GetIata() string {
 	if m != nil {
 		return m.Iata
 	}
 	return ""
 }
 
+type ICAO struct {
+	Icao                 string   `protobuf:"bytes,1,opt,name=icao,proto3" json:"icao,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ICAO) Reset()         { *m = ICAO{} }
+func (m *ICAO) String() string { return proto.CompactTextString(m) }
+func (*ICAO) ProtoMessage()    {}
+func (*ICAO) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ff5d7323532d9adb, []int{4}
+}
+
+func (m *ICAO) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ICAO.Unmarshal(m, b)
+}
+func (m *ICAO) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ICAO.Marshal(b, m, deterministic)
+}
+func (m *ICAO) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ICAO.Merge(m, src)
+}
+func (m *ICAO) XXX_Size() int {
+	return xxx_messageInfo_ICAO.Size(m)
+}
+func (m *ICAO) XXX_DiscardUnknown() {
+	xxx_messageInfo_ICAO.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ICAO proto.InternalMessageInfo
+
+func (m *ICAO) GetIcao() string {
+	if m != nil {
+		return m.Icao
+	}
+	return ""
+}
+
+type AirportDescriptor struct {
+	Descriptor_          string   `protobuf:"bytes,1,opt,name=descriptor,proto3" json:"descriptor,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *AirportDescriptor) Reset()         { *m = AirportDescriptor{} }
+func (m *AirportDescriptor) String() string { return proto.CompactTextString(m) }
+func (*AirportDescriptor) ProtoMessage()    {}
+func (*AirportDescriptor) Descriptor() ([]byte, []int) {
+	return fileDescriptor_ff5d7323532d9adb, []int{5}
+}
+
+func (m *AirportDescriptor) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AirportDescriptor.Unmarshal(m, b)
+}
+func (m *AirportDescriptor) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AirportDescriptor.Marshal(b, m, deterministic)
+}
+func (m *AirportDescriptor) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AirportDescriptor.Merge(m, src)
+}
+func (m *AirportDescriptor) XXX_Size() int {
+	return xxx_messageInfo_AirportDescriptor.Size(m)
+}
+func (m *AirportDescriptor) XXX_DiscardUnknown() {
+	xxx_messageInfo_AirportDescriptor.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_AirportDescriptor proto.InternalMessageInfo
+
+func (m *AirportDescriptor) GetDescriptor_() string {
+	if m != nil {
+		return m.Descriptor_
+	}
+	return ""
+}
+
 func init() {
+	proto.RegisterType((*EmptyRequest)(nil), "iatafinder.EmptyRequest")
 	proto.RegisterType((*Airport)(nil), "iatafinder.Airport")
-	proto.RegisterType((*Iata)(nil), "iatafinder.Iata")
+	proto.RegisterType((*Airports)(nil), "iatafinder.Airports")
+	proto.RegisterType((*IATA)(nil), "iatafinder.IATA")
+	proto.RegisterType((*ICAO)(nil), "iatafinder.ICAO")
+	proto.RegisterType((*AirportDescriptor)(nil), "iatafinder.AirportDescriptor")
 }
 
 func init() { proto.RegisterFile("iatafinder.proto", fileDescriptor_ff5d7323532d9adb) }
 
 var fileDescriptor_ff5d7323532d9adb = []byte{
-	// 303 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x4c, 0x91, 0x4f, 0x6b, 0xf3, 0x30,
-	0x0c, 0xc6, 0x5f, 0xe7, 0xed, 0x5f, 0x75, 0x2b, 0xc5, 0x65, 0x43, 0x94, 0x1d, 0x4a, 0x4f, 0x3d,
-	0xf5, 0xd0, 0xc1, 0xee, 0x83, 0xc1, 0xd8, 0xb5, 0xdb, 0xbd, 0x78, 0x89, 0xd7, 0x19, 0x52, 0xbb,
-	0x38, 0xca, 0x20, 0xf9, 0xbe, 0xfb, 0x1e, 0x43, 0x76, 0xd2, 0xe4, 0xa6, 0xe7, 0xf7, 0x3c, 0xb2,
-	0x91, 0x04, 0x0b, 0xa3, 0x48, 0x7d, 0x19, 0x9b, 0x69, 0xbf, 0xbb, 0x78, 0x47, 0x4e, 0x42, 0x47,
-	0x36, 0xbf, 0x09, 0x8c, 0x9f, 0x8d, 0xbf, 0x38, 0x4f, 0x72, 0x0e, 0x89, 0xc9, 0x50, 0xac, 0xc5,
-	0x76, 0x78, 0x48, 0x4c, 0x26, 0x25, 0x0c, 0xac, 0x3a, 0x6b, 0x4c, 0xd6, 0x62, 0x3b, 0x3d, 0x84,
-	0x9a, 0x59, 0x6a, 0xa8, 0xc2, 0xff, 0x91, 0x71, 0x2d, 0x11, 0xc6, 0xa9, 0x2b, 0x2d, 0xf9, 0x0a,
-	0x07, 0x01, 0xb7, 0x92, 0xd3, 0xfc, 0x17, 0x0e, 0x63, 0x9a, 0xeb, 0xc0, 0x52, 0xe5, 0x70, 0xd4,
-	0xb0, 0x54, 0x39, 0xb9, 0x82, 0x49, 0xae, 0xc8, 0x50, 0x99, 0x69, 0x1c, 0xaf, 0xc5, 0x56, 0x1c,
-	0xae, 0x5a, 0x3e, 0xc0, 0x34, 0x77, 0xf6, 0x14, 0xcd, 0x49, 0x30, 0x3b, 0xc0, 0x9d, 0x2a, 0x6f,
-	0x3a, 0xa7, 0xb1, 0xb3, 0xd5, 0xec, 0x91, 0x39, 0xeb, 0xda, 0x59, 0x8d, 0x10, 0x7e, 0xbb, 0x6a,
-	0xb9, 0x87, 0xbb, 0x4c, 0x55, 0xb9, 0x39, 0x7d, 0xd3, 0xb1, 0x50, 0x3f, 0xc6, 0x9e, 0x8a, 0x23,
-	0x9b, 0x38, 0x0b, 0xc1, 0x65, 0x6b, 0xbe, 0x47, 0xef, 0xc3, 0x9c, 0x35, 0xef, 0x87, 0x6a, 0xbc,
-	0x09, 0x81, 0x84, 0x6a, 0x9e, 0x84, 0xaa, 0x8b, 0xc6, 0xdb, 0x38, 0x09, 0xd7, 0xf2, 0x1e, 0x46,
-	0x85, 0x2b, 0x7d, 0xaa, 0x71, 0x1e, 0x68, 0xa3, 0x36, 0x2b, 0x18, 0xbc, 0xb5, 0xd3, 0xf3, 0x46,
-	0x44, 0xb7, 0x91, 0xfd, 0x0b, 0xf4, 0x2e, 0x22, 0x9f, 0x60, 0xf6, 0xaa, 0xa9, 0xb9, 0x49, 0x21,
-	0x17, 0xbb, 0xde, 0xfd, 0xf8, 0x89, 0xd5, 0xb2, 0x4f, 0x9a, 0xdc, 0xe6, 0xdf, 0xe7, 0x28, 0x1c,
-	0xf7, 0xf1, 0x2f, 0x00, 0x00, 0xff, 0xff, 0x13, 0x4f, 0x33, 0x26, 0xf0, 0x01, 0x00, 0x00,
+	// 416 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x53, 0xcd, 0x8e, 0xd3, 0x30,
+	0x10, 0xc6, 0xd9, 0x6e, 0xd3, 0x4e, 0x97, 0x68, 0xf1, 0x02, 0xb2, 0x22, 0x40, 0x51, 0x4e, 0xb9,
+	0xb0, 0x87, 0xee, 0x69, 0x4f, 0x28, 0x2a, 0x08, 0x71, 0x5a, 0x29, 0xf4, 0x5e, 0x99, 0xc4, 0x14,
+	0x4b, 0x49, 0x1c, 0x9c, 0x09, 0x52, 0xfa, 0x04, 0xbc, 0x28, 0xef, 0x81, 0xec, 0xfc, 0x22, 0xda,
+	0xdb, 0xcc, 0xf7, 0x33, 0xf6, 0x7c, 0x71, 0xe0, 0x56, 0x72, 0xe4, 0xdf, 0x65, 0x99, 0x09, 0x7d,
+	0x5f, 0x69, 0x85, 0x8a, 0xc2, 0x84, 0x84, 0x1e, 0xdc, 0x7c, 0x2a, 0x2a, 0x6c, 0x13, 0xf1, 0xb3,
+	0x11, 0x35, 0x86, 0x7f, 0x1c, 0x70, 0x63, 0xa9, 0x2b, 0xa5, 0x91, 0x7a, 0xe0, 0xc8, 0x8c, 0x91,
+	0x80, 0x44, 0xd7, 0x89, 0x23, 0x33, 0x4a, 0x61, 0x51, 0xf2, 0x42, 0x30, 0x27, 0x20, 0xd1, 0x3a,
+	0xb1, 0xb5, 0xc1, 0x52, 0x89, 0x2d, 0xbb, 0xea, 0x30, 0x53, 0x53, 0x06, 0x6e, 0xaa, 0x9a, 0x12,
+	0x75, 0xcb, 0x16, 0x16, 0x1e, 0x5a, 0xa3, 0x36, 0x67, 0xb3, 0xeb, 0x4e, 0x6d, 0x6a, 0x8b, 0xa5,
+	0x5c, 0xb1, 0x65, 0x8f, 0xa5, 0x5c, 0x51, 0x1f, 0x56, 0x39, 0x47, 0x89, 0x4d, 0x26, 0x98, 0x1b,
+	0x90, 0x88, 0x24, 0x63, 0x4f, 0xdf, 0xc0, 0x3a, 0x57, 0xe5, 0xb1, 0x23, 0x57, 0x96, 0x9c, 0x00,
+	0xe3, 0xe4, 0x79, 0xef, 0x5c, 0x77, 0xce, 0xa1, 0x37, 0x1c, 0xca, 0x42, 0x9c, 0x54, 0x29, 0x18,
+	0xd8, 0xd3, 0xc6, 0x9e, 0x6e, 0xe1, 0x55, 0xc6, 0xdb, 0x5c, 0x1e, 0x7f, 0xe0, 0xa1, 0xe6, 0xbf,
+	0x64, 0x79, 0xac, 0x0f, 0x86, 0x64, 0x1b, 0x2b, 0xbc, 0x1b, 0xc8, 0xaf, 0x1d, 0xb7, 0x97, 0x85,
+	0x30, 0xf9, 0xe0, 0x89, 0xdd, 0x58, 0x81, 0x83, 0x27, 0xb3, 0x09, 0xb6, 0x95, 0x60, 0xcf, 0xbb,
+	0x4d, 0x4c, 0x4d, 0x5f, 0xc3, 0xb2, 0x56, 0x8d, 0x4e, 0x05, 0xf3, 0x2c, 0xda, 0x77, 0xe1, 0x23,
+	0xac, 0xfa, 0x98, 0x6b, 0xfa, 0x1e, 0x5c, 0xde, 0xd5, 0x8c, 0x04, 0x57, 0xd1, 0x66, 0x7b, 0x77,
+	0x3f, 0xfb, 0x66, 0xbd, 0x2c, 0x19, 0x34, 0xa1, 0x0f, 0x8b, 0x2f, 0xf1, 0x3e, 0x1e, 0xc3, 0x24,
+	0x53, 0x98, 0x96, 0xdb, 0xc5, 0x4f, 0x63, 0xa8, 0x64, 0x0a, 0x35, 0x7c, 0x80, 0x17, 0xfd, 0xac,
+	0x8f, 0xa2, 0x4e, 0xb5, 0xac, 0x50, 0x69, 0xfa, 0x0e, 0x20, 0x1b, 0xbb, 0x5e, 0x3e, 0x43, 0xb6,
+	0xbf, 0x1d, 0x98, 0x3d, 0x17, 0xfa, 0x01, 0x36, 0x9f, 0x05, 0x8e, 0x37, 0x67, 0xf3, 0x8b, 0xce,
+	0xdf, 0x91, 0xff, 0xf2, 0xcc, 0x0a, 0x75, 0xf8, 0x8c, 0xee, 0x00, 0xa6, 0x01, 0xf4, 0xed, 0x19,
+	0xd5, 0x74, 0xb9, 0x8b, 0x43, 0x1e, 0xc1, 0x9b, 0x86, 0xd8, 0x2c, 0x6e, 0xe7, 0x4a, 0x83, 0xf8,
+	0xe7, 0x32, 0xfc, 0xcf, 0x6a, 0xa2, 0xfa, 0xd7, 0xba, 0x8b, 0x9f, 0x2e, 0x58, 0xbf, 0x2d, 0xed,
+	0xdf, 0xf3, 0xf0, 0x37, 0x00, 0x00, 0xff, 0xff, 0xe2, 0xa8, 0x05, 0x24, 0x51, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -246,7 +407,10 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type IatafinderClient interface {
-	GetAirports(ctx context.Context, in *Iata, opts ...grpc.CallOption) (*Airport, error)
+	GetAirports(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*Airports, error)
+	GetAirport(ctx context.Context, in *AirportDescriptor, opts ...grpc.CallOption) (*Airports, error)
+	GetAirportIATA(ctx context.Context, in *IATA, opts ...grpc.CallOption) (*Airport, error)
+	GetAirportICAO(ctx context.Context, in *ICAO, opts ...grpc.CallOption) (*Airport, error)
 }
 
 type iatafinderClient struct {
@@ -257,9 +421,36 @@ func NewIatafinderClient(cc *grpc.ClientConn) IatafinderClient {
 	return &iatafinderClient{cc}
 }
 
-func (c *iatafinderClient) GetAirports(ctx context.Context, in *Iata, opts ...grpc.CallOption) (*Airport, error) {
-	out := new(Airport)
+func (c *iatafinderClient) GetAirports(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*Airports, error) {
+	out := new(Airports)
 	err := c.cc.Invoke(ctx, "/iatafinder.iatafinder/GetAirports", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *iatafinderClient) GetAirport(ctx context.Context, in *AirportDescriptor, opts ...grpc.CallOption) (*Airports, error) {
+	out := new(Airports)
+	err := c.cc.Invoke(ctx, "/iatafinder.iatafinder/GetAirport", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *iatafinderClient) GetAirportIATA(ctx context.Context, in *IATA, opts ...grpc.CallOption) (*Airport, error) {
+	out := new(Airport)
+	err := c.cc.Invoke(ctx, "/iatafinder.iatafinder/GetAirportIATA", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *iatafinderClient) GetAirportICAO(ctx context.Context, in *ICAO, opts ...grpc.CallOption) (*Airport, error) {
+	out := new(Airport)
+	err := c.cc.Invoke(ctx, "/iatafinder.iatafinder/GetAirportICAO", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -268,7 +459,27 @@ func (c *iatafinderClient) GetAirports(ctx context.Context, in *Iata, opts ...gr
 
 // IatafinderServer is the server API for Iatafinder service.
 type IatafinderServer interface {
-	GetAirports(context.Context, *Iata) (*Airport, error)
+	GetAirports(context.Context, *EmptyRequest) (*Airports, error)
+	GetAirport(context.Context, *AirportDescriptor) (*Airports, error)
+	GetAirportIATA(context.Context, *IATA) (*Airport, error)
+	GetAirportICAO(context.Context, *ICAO) (*Airport, error)
+}
+
+// UnimplementedIatafinderServer can be embedded to have forward compatible implementations.
+type UnimplementedIatafinderServer struct {
+}
+
+func (*UnimplementedIatafinderServer) GetAirports(ctx context.Context, req *EmptyRequest) (*Airports, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAirports not implemented")
+}
+func (*UnimplementedIatafinderServer) GetAirport(ctx context.Context, req *AirportDescriptor) (*Airports, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAirport not implemented")
+}
+func (*UnimplementedIatafinderServer) GetAirportIATA(ctx context.Context, req *IATA) (*Airport, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAirportIATA not implemented")
+}
+func (*UnimplementedIatafinderServer) GetAirportICAO(ctx context.Context, req *ICAO) (*Airport, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAirportICAO not implemented")
 }
 
 func RegisterIatafinderServer(s *grpc.Server, srv IatafinderServer) {
@@ -276,7 +487,7 @@ func RegisterIatafinderServer(s *grpc.Server, srv IatafinderServer) {
 }
 
 func _Iatafinder_GetAirports_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Iata)
+	in := new(EmptyRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -288,7 +499,61 @@ func _Iatafinder_GetAirports_Handler(srv interface{}, ctx context.Context, dec f
 		FullMethod: "/iatafinder.iatafinder/GetAirports",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IatafinderServer).GetAirports(ctx, req.(*Iata))
+		return srv.(IatafinderServer).GetAirports(ctx, req.(*EmptyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Iatafinder_GetAirport_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AirportDescriptor)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(IatafinderServer).GetAirport(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/iatafinder.iatafinder/GetAirport",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(IatafinderServer).GetAirport(ctx, req.(*AirportDescriptor))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Iatafinder_GetAirportIATA_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IATA)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(IatafinderServer).GetAirportIATA(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/iatafinder.iatafinder/GetAirportIATA",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(IatafinderServer).GetAirportIATA(ctx, req.(*IATA))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Iatafinder_GetAirportICAO_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ICAO)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(IatafinderServer).GetAirportICAO(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/iatafinder.iatafinder/GetAirportICAO",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(IatafinderServer).GetAirportICAO(ctx, req.(*ICAO))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -300,6 +565,18 @@ var _Iatafinder_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetAirports",
 			Handler:    _Iatafinder_GetAirports_Handler,
+		},
+		{
+			MethodName: "GetAirport",
+			Handler:    _Iatafinder_GetAirport_Handler,
+		},
+		{
+			MethodName: "GetAirportIATA",
+			Handler:    _Iatafinder_GetAirportIATA_Handler,
+		},
+		{
+			MethodName: "GetAirportICAO",
+			Handler:    _Iatafinder_GetAirportICAO_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
